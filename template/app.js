cd3b57deb729ceb13843a3814766f8e7c1cc0ab5
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-11-16 09:24:21 +0000

    Moviendo ficheros

--- template/app.js
+++ template/app.js
@@ -1,19 +1,17 @@
 // importar
     var express = require('express');
-    var deploy-heroku = require(path.join(__dirname,'../gulpfile.js'));
-    // instanciar
     var app = express();
     var path = require('path');
-    
-     
+
+
     // escuchar
    // app.listen(8080);
    app.set('port', process.env.PORT || 8080);
 
     deploy-heroku.push_heroku();
-          
 
-//heroku git:remote -a heroku-project-name    
+
+//heroku git:remote -a heroku-project-name
 app.listen(app.get('port'), function() {
   console.log('Servidor escuchando en el puerto:'+app.get('port'));
 });
