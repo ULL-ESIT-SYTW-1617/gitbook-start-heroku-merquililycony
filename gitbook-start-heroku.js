5a9a872cb5f16d4ae440511c4581e7ab906deaf4
Cony <alu0100673647@ull.edu.es>
2016-11-18 02:59:28 +0000

    Body.id

--- gitbook-start-heroku.js
+++ gitbook-start-heroku.js
@@ -2,14 +2,25 @@ var github = require('octonode');
 var client = github.client();
 const readline = require('readline');
 var readlineSync = require('readline-sync');
+var fs = require('fs');
 
-/********** Autenticacion usuario *********************************/
+
 var usuario = readlineSync.question('Introduzca el USUARIO de github: ');
+
+var id_;
 var pass = readlineSync.question('Introduzca contraseña de github: ');
 var ghuser;
 client.get('/users/'+usuario, {}, function (err, status, body, headers) {
+ 
+      
+       
   //console.log(body); //json object
    ghuser = client.user(usuario);
+   console.log(body); //json object
+  console.log("BODY NAME")
+  console.log(body.name); //json object
+  id_ = body.id;
+  console.log(id_);
 // console.log("HOLA: ", ghuser);
 });
 
@@ -19,13 +30,11 @@ var client = github.client({
   password: pass
 });
 
-/**********************************************************************/
+
 client.get('/user', {}, function (err, status, body, headers) {
-  console.log(body); //json object
+  //console.log(body); //json object
 });
 var client = github.client('someaccesstoken');
 
-client.get('/user', {}, function (err, status, body, headers) {
- // console.log(body); //json object
-});
+
 
