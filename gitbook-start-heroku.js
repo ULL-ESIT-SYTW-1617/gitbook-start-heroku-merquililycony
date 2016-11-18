34920cf2ae80d6a0ce9a3ff05f14d8ae12df5570
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-11-18 06:13:03 +0000

    Token funciona correctamente

--- gitbook-start-heroku.js
+++ gitbook-start-heroku.js
@@ -3,6 +3,9 @@ var client = github.client();
 const readline = require('readline');
 var readlineSync = require('readline-sync');
 var fs = require('fs');
+var findup = require('findup-sync');
+//var objects = require('./objects');
+
 
 
 var usuario = readlineSync.question('Introduzca el USUARIO de github: ');
@@ -10,17 +13,24 @@ var usuario = readlineSync.question('Introduzca el USUARIO de github: ');
 var id_;
 var pass = readlineSync.question('Introduzca contraseña de github: ');
 var ghuser;
+
+//COGER TOKEN
+  var json = JSON.parse(fs.readFileSync('mitoken.json','utf8'))
+  var token = json.token;
+  console.log("MI TOKEN");
+  console.log(token);
+
 client.get('/users/'+usuario, {}, function (err, status, body, headers) {
- 
-      
-       
+
+
+
   //console.log(body); //json object
    ghuser = client.user(usuario);
-   console.log(body); //json object
-  console.log("BODY NAME")
-  console.log(body.name); //json object
-  id_ = body.id;
-  console.log(id_);
+  // console.log(body); //json object
+  //console.log("BODY NAME")
+//  console.log(body.name); //json object
+//  id_ = body.id;
+//  console.log(id_);
 // console.log("HOLA: ", ghuser);
 });
 
@@ -35,6 +45,3 @@ client.get('/user', {}, function (err, status, body, headers) {
   //console.log(body); //json object
 });
 var client = github.client('someaccesstoken');
-
-
-
