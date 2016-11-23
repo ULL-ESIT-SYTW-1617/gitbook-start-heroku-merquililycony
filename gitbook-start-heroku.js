b19f6ddfd17c751e8e5d730df22540360d7ea741
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-11-23 23:33:29 +0000

    añadiendo comentario

--- gitbook-start-heroku.js
+++ gitbook-start-heroku.js
@@ -14,36 +14,34 @@ var usuario = readlineSync.question('Introduzca el USUARIO de github: ');
 
 var mifuncion = function(){
 
-  //var args = " -i -u alu0100536690 -d ";
-  var args = " -i -u "+usuario+" -d ";
-  var args1 = '\'{"scopes": ["repo", "user"], "note":"mlc"}\'';
-  var args2 = " https://api.github.com/authorizations >> mlc.json";
-  var crear_token = args + args1 + args2;
+//var args = " -i -u alu0100536690 -d ";
+var args = " -i -u "+usuario+" -d ";
+var args1 = '\'{"scopes": ["repo", "user"], "note":"mlc"}\'';
+var args2 = " https://api.github.com/authorizations >> mlc.json";
+var crear_token = args + args1 + args2;
 
+  //TENEMOS QUE HACER UN CALLBACK, PROMESA O UTILIZAR LA LIBRERÍA Async y Await
+  //PARA QUE NO CONTINUE HASTA QUE NO FINALICE LA FUNCIÓN
+   var workerProcess = child_process.exec('curl ' + crear_token,function
+      (error, stdout, stderr) {
 
-  console.log("Introduzca su contraseña de github: ")
+      if (error) {
+         console.log(error.stack);
+         console.log('Error code: '+error.code);
+         console.log('Signal received: '+error.signal);
+      }
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      child_process.exec('sed -i "1,25d" mlc.json');//elimina de la linea 1 a la 25
 
-
-     var workerProcess = child_process.exec('curl ' + crear_token,function
-        (error, stdout, stderr) {
-
-        if (error) {
-           console.log(error.stack);
-           console.log('Error code: '+error.code);
-           console.log('Signal received: '+error.signal);
-        }
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        child_process.exec('sed -i "1,25d" mlc.json');//elimina de la linea 1 a la 25
-
-     });
-        workerProcess.on('exit', function (code) {
-        console.log('Child process exited with exit code '+code);
-     });
+   });
+      workerProcess.on('exit', function (code) {
+      console.log('Child process exited with exit code '+code);
+   });
 
 
 
-  console.log("fin exec");
+console.log("fin exec");
 };
 
 mifuncion();
@@ -61,20 +59,20 @@ console.log(token);
 client.get('/users/'+usuario, {}, function (err, status, body, headers) {
 
 
- var ghuser;
-  //console.log(body); //json object
-  ghuser = client.user(usuario);
-  // console.log(body); //json object
-  //console.log("BODY NAME")
-  //  console.log(body.name); //json object
-  //  id_ = body.id;
-  //  console.log(id_);
-  // console.log("HOLA: ", ghuser);
+var ghuser;
+//console.log(body); //json object
+ghuser = client.user(usuario);
+// console.log(body); //json object
+//console.log("BODY NAME")
+//  console.log(body.name); //json object
+//  id_ = body.id;
+//  console.log(id_);
+// console.log("HOLA: ", ghuser);
 });
 
 
 
 client.get('/user', {}, function (err, status, body, headers) {
-  //console.log(body); //json object
+//console.log(body); //json object
 });
 var client = github.client('someaccesstoken');
