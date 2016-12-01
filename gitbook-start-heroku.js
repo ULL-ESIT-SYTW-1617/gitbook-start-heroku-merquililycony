345ed36e3dc393f1fb1db37a37a6f72ecd642229
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-12-01 22:22:07 +0000

    cambio version

--- gitbook-start-heroku.js
+++ gitbook-start-heroku.js
@@ -24,7 +24,7 @@ var password = readlineSync.question('Introduzca su contraseña de github: ', { 
 // //COGER TOKEN
 // var json = JSON.parse(fs.readFileSync(usuario+'.json','utf8'))
 
- var json1 = JSON.parse(fs.readFileSync('../../package.json','utf8'));
+ var json1 = JSON.parse(fs.readFileSync('./../../package.json','utf8'));
  json1.repository.url = "abcd";
  // var d = json1.dir_name;
  // json1.repository.url = "https://github.com/"+usuario+"/"+d+".git >> nuevo.json";
